7aa46606aeb710e019ccdb12c2bac2d0822ef1f6
Florian Besser <flbe@zuehlke.com>
2017-01-28 22:05:26 +0100

    Beautified

--- contract/debug.sol
+++ contract/debug.sol
@@ -2,6 +2,6 @@ pragma solidity ^0.4.4;
 
 contract Auth {      
     function verify( bytes32 hash, uint8 v, bytes32 r, bytes32 s) constant returns(address retAddr) {
-        retAddr= ecrecover(hash, v, r, s);
+        retAddr = ecrecover(hash, v, r, s);
     }
 }
