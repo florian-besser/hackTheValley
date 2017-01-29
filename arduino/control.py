cd76961e56ccc1b64a58bd80a489b15a6b67eb8a
Daniel Scheu <daniel@heuphone.home>
2017-01-29 11:40:43 +0100

    Set new id

--- arduino/control.py
+++ arduino/control.py
@@ -7,7 +7,7 @@ while True:
 	print command
 	if command.startswith("0x"):
 		print "Verifying in Blockchain if user has access to parking lot..."
-		out, err = subprocess.Popen(['geth','--preload','/Users/daniel/hackathon/hackTheValley/norsborg-client/test.js','--exec','contract.hasAccess(123, "' + command + '")','attach', 'ipc:/Users/daniel/.ethereum-norsborg-data-16123/geth.ipc'], stdout=subprocess.PIPE).communicate()
+		out, err = subprocess.Popen(['geth','--preload','/Users/daniel/hackathon/hackTheValley/norsborg-client/test.js','--exec','contract.hasAccess(125, "' + command + '")','attach', 'ipc:/Users/daniel/.ethereum-norsborg-data-16123/geth.ipc'], stdout=subprocess.PIPE).communicate()
 		response = out.strip()
 		print "Blockchain said: "
 		print response
